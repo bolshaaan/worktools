459df1ab065dc7f65a79d42ccbb83c2aa18522b9
alexander.bolshakov <alexander.bolshakov@ncloudtech.ru>
2018-03-02 19:41:30 +0300

    more flexible

--- kctl.sh
+++ kctl.sh
@@ -3,9 +3,15 @@
 SCRUM=$1
 CMD=$2
 
-echo "SCRUM: $SCRUM; CMD: $CMD"
+WHAT=$3
 
-if [ $# -ne 2 ]; then
+if [ -z "${WHAT}" ]; then
+	WHAT=sh;
+fi
+
+echo "SCRUM: $SCRUM; CMD: $CMD; WHAT: $WHAT"
+
+if [ $# -le 1 ]; then
 	echo "use following:  kctl.sh scrumN (logs|ca|ru)";
 	exit 0;
 fi
@@ -18,10 +24,23 @@ elif [ $CMD = "ca" ]; then
 	KPOD=`kubectl --namespace ${SCRUM} get pod | perl -ne ' print $1 if /(class-availability-\d+\w+-\w+)/; '`
 	printf '\033[31m%s\033[0m\n\n\n'  "<=== Making SSH to $KPOD ===>"
 	kubectl --namespace $SCRUM exec -it $KPOD -c class-availability sh
-else [ $CMD = "ru" ]
+elif [ $CMD = "ru" ]; then
 	KPOD=`kubectl --namespace ${SCRUM} get pod | perl -ne ' print $1 if /(ru-\d+\w+-\w+)/; '`
 	printf '\033[31m%s\033[0m\n\n\n'  "<=== Making SSH to $KPOD ===>"
 	kubectl --namespace $SCRUM exec -it $KPOD -c ru sh
+else
+
+
+	KPOD=`kubectl --namespace ${SCRUM} get pod | CMD=$CMD perl -ne ' print $1 if /($ENV{CMD}-\d+\w+-\w+)/; '`
+	printf '\033[31m%s\033[0m\n\n\n'  "<=== Making SSH to $KPOD $CMD ===>"
+
+	if [ $WHAT = logs ]; then
+		kubectl --namespace $SCRUM logs  $KPOD  $CMD -f
+	else
+		kubectl --namespace $SCRUM exec -it $KPOD -c $CMD $WHAT
+	fi
+
 fi
 
+# elif [ $CMD = "publicapi" ]; then
 
