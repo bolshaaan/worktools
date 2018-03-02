0412f19e7e22a1377bd6262d2b399e64cd0f536d
alexander.bolshakov <alexander.bolshakov@ncloudtech.ru>
2018-03-02 11:38:54 +0300

    more flexible

--- kctl.sh
+++ kctl.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
+SCRUM=$1
+CMD=$2
 
-echo "PARAM: $1"
+echo "SCRUM: $SCRUM; CMD: $CMD"
 
-if [ $1 = "logs" ]; then
-	KPOD=`kubectl --namespace scrum50 get pod | perl -ne ' print $1 if /(employee-restrictions-\d+\w+-\w+)/; '`
-	printf '\033[31m%s\033[0m\n\n\n\n'  "<=== Making logs from $KPOD ===>"
-	kubectl --namespace scrum50 logs  $KPOD  employee-restrictions -f
-elif [ $1 = "ca" ]; then
-	KPOD=`kubectl --namespace scrum50 get pod | perl -ne ' print $1 if /(class-availability-\d+\w+-\w+)/; '`
-	printf '\033[31m%s\033[0m\n\n\n'  "<=== Making SSH to $KPOD ===>"
-	kubectl --namespace scrum50 exec -it $KPOD -c class-availability sh
-else
-	KPOD=`kubectl --namespace scrum50 get pod | perl -ne ' print $1 if /(ru-\d+\w+-\w+)/; '`
-	printf '\033[31m%s\033[0m\n\n\n'  "<=== Making SSH to $KPOD ===>"
-	kubectl --namespace scrum50 exec -it $KPOD -c ru sh
+if [ -z "$2" ]; then
+	echo "use fullowing:  kctl.sh scrumN (logs|ca|ru)";
+	exit 0;
 fi
+
+
+if [ $CMD = "logs" ]; then
+	KPOD=`kubectl --namespace ${SCRUM} get pod | perl -ne ' print $1 if /(employee-restrictions-\d+\w+-\w+)/; '`
+	printf '\033[31m%s\033[0m\n\n\n\n'  "<=== Making logs from $KPOD ===>"
+	kubectl --namespace $SCRUM logs  $KPOD  employee-restrictions -f
+elif [ $CMD = "ca" ]; then
+	KPOD=`kubectl --namespace ${SCRUM} get pod | perl -ne ' print $1 if /(class-availability-\d+\w+-\w+)/; '`
+	printf '\033[31m%s\033[0m\n\n\n'  "<=== Making SSH to $KPOD ===>"
+	kubectl --namespace $SCRUM exec -it $KPOD -c class-availability sh
+else [ $CMD = "ru" ]
+	KPOD=`kubectl --namespace ${SCRUM} get pod | perl -ne ' print $1 if /(ru-\d+\w+-\w+)/; '`
+	printf '\033[31m%s\033[0m\n\n\n'  "<=== Making SSH to $KPOD ===>"
+	kubectl --namespace $SCRUM exec -it $KPOD -c ru sh
+fi
+
 
